2d865c4deb4918824391e0123326e9f58445b107
wangwenfeng <sxian.wang@gmail.com>
2019-02-17 23:17:19 +0800

    Add implementation of RocketMQ protocol in pure go

---
+++ remote/client.go
@@ -0,0 +1,277 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package remote
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	log "github.com/sirupsen/logrus"
+	"net"
+	"sync"
+	"time"
+)
+
+var (
+	ErrRequestTimeout = errors.New("request timeout")
+)
+
+type RemotingClient interface {
+	InvokeSync(request *remotingCommand) (*remotingCommand, error)
+	InvokeAsync(request *remotingCommand, f func(*remotingCommand)) error
+	InvokeOneWay(request *remotingCommand) error
+}
+
+
+// ClientConfig common config
+type ClientConfig struct {
+	// NameServer or Broker address
+	RemotingAddress string
+
+	ClientIP     string
+	InstanceName string
+
+	// Heartbeat interval in microseconds with message broker, default is 30
+	HeartbeatBrokerInterval time.Duration
+
+	// request timeout time
+	RequestTimeout time.Duration
+	CType byte
+
+	UnitMode          bool
+	UnitName          string
+	VipChannelEnabled bool
+}
+
+type defaultClient struct {
+	//clientId     string
+	config ClientConfig
+	conn net.Conn
+	// requestId
+	opaque int32
+
+	// int32 -> ResponseFuture
+	responseTable sync.Map
+	codec         serializer
+	exitCh chan interface{}
+}
+
+func NewRemotingClient(config ClientConfig) (RemotingClient, error) {
+	client := &defaultClient{
+		config: config,
+	}
+
+	switch config.CType {
+	case Json:
+		client.codec = &jsonCodec{}
+	case RocketMQ:
+		client.codec = &rmqCodec{}
+	default:
+		return nil, errors.New("unknow codec")
+	}
+
+	conn, err := net.Dial("tcp", config.RemotingAddress)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	client.conn = conn
+	go client.listen()
+	go client.clearExpiredRequest()
+	return client, nil
+}
+
+func (client *defaultClient) InvokeSync(request *remotingCommand) (*remotingCommand, error) {
+
+	response := &ResponseFuture{
+		SendRequestOK:  false,
+		Opaque:         request.Opaque,
+		TimeoutMillis:  client.config.RequestTimeout,
+		BeginTimestamp: time.Now().Unix(),
+		Done:           make(chan bool),
+	}
+	header, err := encode(request)
+	body := request.Body
+	client.responseTable.Store(request.Opaque, response)
+	err = client.doRequest(header, body)
+
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	select {
+	case <-response.Done:
+		rmd := response.ResponseCommand
+		return rmd, nil
+	case <-time.After(client.config.RequestTimeout):
+		return nil, ErrRequestTimeout
+	}
+}
+
+func (client *defaultClient) InvokeAsync(request *remotingCommand, f func(*remotingCommand)) error {
+
+	response := &ResponseFuture{
+		SendRequestOK:  false,
+		Opaque:         request.Opaque,
+		TimeoutMillis:  client.config.RequestTimeout,
+		BeginTimestamp: time.Now().Unix(),
+		callback:       f,
+	}
+	client.responseTable.Store(request.Opaque, response)
+	header, err := encode(request)
+	if err != nil {
+		return err
+	}
+
+	body := request.Body
+	return client.doRequest(header, body)
+}
+
+func (client *defaultClient) InvokeOneWay(request *remotingCommand) error {
+	header, err := encode(request)
+	if err != nil {
+		return err
+	}
+
+	body := request.Body
+	return client.doRequest(header, body)
+}
+
+func (client *defaultClient) doRequest(header, body []byte) error {
+	var requestBytes []byte
+	requestBytes = append(requestBytes, header...)
+	if body != nil && len(body) > 0 {
+		requestBytes = append(requestBytes, body...)
+	}
+	_, err := client.conn.Write(requestBytes)
+	return err
+}
+
+func (client *defaultClient) close() {
+	// TODO process response
+	client.conn.Close()
+}
+
+func (client *defaultClient) listen() {
+	b := make([]byte, 1024)
+	var length, headerLength, bodyLength int32
+	var buf = bytes.NewBuffer([]byte{})
+	var header, body []byte
+	var readTotalLengthFlag = true // readLen when true, read data when false
+	for {
+		var n int
+		n, err := client.conn.Read(b)
+		if err != nil {
+			return
+		}
+		_, err = buf.Write(b[:n])
+		if err != nil {
+			return
+		}
+		for {
+			if readTotalLengthFlag {
+				//we read 4 bytes of allDataLength
+				if buf.Len() >= 4 {
+					err = binary.Read(buf, binary.BigEndian, &length)
+					if err != nil {
+						return
+					}
+					readTotalLengthFlag = false //now turn to read data
+				} else {
+					break //wait bytes we not got
+				}
+			}
+			if !readTotalLengthFlag {
+				if buf.Len() < int(length) {
+					// judge all data received.if not,loop to wait
+					break
+				}
+			}
+			//now all data received, we can read totalLen again
+			readTotalLengthFlag = true
+
+			//get the data,and handler it
+			//header len
+			err = binary.Read(buf, binary.BigEndian, &headerLength)
+			var realHeaderLen = headerLength & 0x00ffffff
+			//headerData the first ff is about serializable type
+			var headerSerializableType = byte(headerLength >> 24)
+			header = make([]byte, realHeaderLen)
+			_, err = buf.Read(header)
+			bodyLength = length - 4 - realHeaderLen
+			body = make([]byte, int(bodyLength))
+			if bodyLength == 0 {
+				// no body
+			} else {
+				_, err = buf.Read(body)
+			}
+			go client.handlerReceivedMessage(client.conn, headerSerializableType, header, body)
+		}
+	}
+}
+
+func (client *defaultClient) handlerReceivedMessage(conn net.Conn, headerSerializableType byte, headBytes []byte, bodyBytes []byte) {
+	//cmd, _ := decode(headBytes, bodyBytes)
+	//if cmd.isResponseType() {
+	//	client.handlerResponse(cmd)
+	//	return
+	//}
+	//go client.handlerRequestFromServer(cmd)
+}
+
+func (client *defaultClient) handlerRequestFromServer(cmd *remotingCommand) {
+	//responseCommand := client.clientRequestProcessor(cmd)
+	//if responseCommand == nil {
+	//	return
+	//}
+	//responseCommand.Opaque = cmd.Opaque
+	//responseCommand.markResponseType()
+	//header, err := encode(responseCommand)
+	//body := responseCommand.Body
+	//err = client.doRequest(header, body)
+	//if err != nil {
+	//	log.Error(err)
+	//}
+}
+
+func (client *defaultClient) handlerResponse(cmd *remotingCommand) error {
+	//response, err := client.getResponse(cmd.Opaque)
+	////client.removeResponse(cmd.Opaque)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//response.ResponseCommand = cmd
+	//response.callback(cmd)
+	//
+	//if response.Done != nil {
+	//	response.Done <- true
+	//}
+	return nil
+}
+
+func (client *defaultClient) clearExpiredRequest() {
+	//for seq, responseObj := range client.responseTable.Items() {
+	//	response := responseObj.(*ResponseFuture)
+	//	if (response.BeginTimestamp + 30) <= time.Now().Unix() {
+	//		//30 minutes expired
+	//		client.responseTable.Remove(seq)
+	//		response.callback(nil)
+	//		log.Warningf("remove time out request %v", response)
+	//	}
+	//}
+}
---
+++ remote/codec.go
@@ -0,0 +1,431 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package remote
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/json"
+	"sync/atomic"
+)
+
+var opaque int32
+
+const (
+	// 0, REQUEST_COMMAND
+	RpcType = 0
+
+	// 1, RPC
+	RpcOneWay = 1
+
+	ResponseType = 1
+
+	RemotingCommandFlag    = 0
+	languageFlag           = "golang"
+	languageCode = byte(9)
+	remotingCommandVersion = 137
+)
+
+type remotingCommand struct {
+	Code      int16             `json:"code"`
+	Language  string            `json:"language"`
+	Version   int16             `json:"version"`
+	Opaque    int32             `json:"opaque"`
+	Flag      int               `json:"flag"`
+	Remark    string            `json:"remark"`
+	ExtFields map[string]string `json:"extFields"`
+	Body      []byte            `json:"body"`
+}
+
+func newRemotingCommand(code int16, properties map[string]string, body []byte) *remotingCommand {
+	remotingCommand := &remotingCommand{
+		Code:      code,
+		Language:  languageFlag,
+		Version:   remotingCommandVersion,
+		Opaque:    atomic.AddInt32(&opaque, 1),
+		ExtFields: properties,
+		Body:      body,
+	}
+
+	return remotingCommand
+}
+
+func (command *remotingCommand) isResponseType() bool {
+	return command.Flag&(ResponseType) == ResponseType
+}
+
+func (command *remotingCommand) markResponseType() {
+	command.Flag = command.Flag | ResponseType
+}
+
+var (
+	jsonSerializer = &jsonCodec{}
+	rocketMqSerializer = &rmqCodec{}
+	codecType byte
+)
+
+// encode remotingCommand
+//
+// Frame format:
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// + item | frame_size | header_length |         header_body        |     body     +
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// + len  |   4bytes   |     4bytes    | (19 + r_len + e_len) bytes | remain bytes +
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+func encode(command *remotingCommand) ([]byte, error) {
+	var (
+		header []byte
+		err error
+	)
+
+	switch codecType {
+	case Json:
+		header, err  = jsonSerializer.encodeHeader(command)
+	case RocketMQ:
+		header, err  = rocketMqSerializer.encodeHeader(command)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	frameSize := 8 + len(header) + len(command.Body)
+	buf := bytes.NewBuffer(make([]byte, frameSize))
+
+	err = binary.Write(buf, binary.BigEndian, int32(frameSize))
+	if err != nil {
+		return nil, err
+	}
+
+	err = binary.Write(buf, binary.BigEndian, markProtocolType(int32(len(header))))
+	if err != nil {
+		return nil, err
+	}
+
+	err = binary.Write(buf, binary.BigEndian, int32(len(command.Body)))
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func decode(data []byte) (*remotingCommand, error) {
+	buf := bytes.NewBuffer(data)
+
+	var oriHeaderLen, headerLength int32
+	err := binary.Read(buf, binary.BigEndian, &oriHeaderLen)
+	if err != nil {
+		return nil, err
+	}
+	headerLength = oriHeaderLen & 0xFFFFFF
+
+	headerData := make([]byte, headerLength)
+	err = binary.Read(buf, binary.BigEndian, &headerLength)
+	if err != nil {
+		return nil, err
+	}
+
+	var command *remotingCommand
+
+	switch byte((oriHeaderLen >> 24) & 0xFF) {
+	case Json:
+		command, err = jsonSerializer.decodeHeader(headerData)
+	case RocketMQ:
+		command, err = rocketMqSerializer.decodeHeader(headerData)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	bodyLength := int32(len(data)) - 4 - headerLength
+	command.Body = make([]byte, bodyLength)
+	err = binary.Read(buf, binary.BigEndian, &command.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return command, nil
+}
+
+func markProtocolType(source int32) []byte {
+	result := make([]byte, 4)
+	result[0] = codecType
+	result[1] = byte((source >> 16) & 0xFF)
+	result[2] = byte((source >> 8) & 0xFF)
+	result[3] = byte(source & 0xFF)
+	return result
+}
+
+const (
+	Json  = byte(0)
+	RocketMQ  = byte(1)
+)
+
+type serializer interface {
+	encodeHeader(command *remotingCommand) ([]byte, error)
+	decodeHeader(data []byte) (*remotingCommand, error)
+}
+
+// jsonCodec please refer to remoting/protocol/RemotingSerializable
+type jsonCodec struct {}
+
+func (c *jsonCodec) encodeHeader(command *remotingCommand) ([]byte, error) {
+	buf, err := json.Marshal(command)
+	if err != nil {
+		return nil, err
+	}
+	return buf, nil
+}
+
+func (c *jsonCodec) decodeHeader(header []byte) (*remotingCommand, error) {
+	command := &remotingCommand{}
+	command.ExtFields = make(map[string]string)
+	err := json.Unmarshal(header, command)
+	if err != nil {
+		return nil, err
+	}
+	return command, nil
+}
+
+// rmqCodec implementation of RocketMQ private protocol, please refer to remoting/protocol/RocketMQSerializable
+// RocketMQ Private Protocol Header format:
+//
+// v_flag: version flag
+// r_len: length of remark body
+// r_body: data of remark body
+// e_len: length of extends fields body
+// e_body: data of extends fields
+//
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// + item | request_code | l_flag | v_flag | opaque | request_flag |  r_len  |   r_body    |  e_len  |    e_body   +
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// + len  |    2bytes    |  1byte | 2bytes | 2bytes |    4 bytes   | 4 bytes | r_len bytes | 4 bytes | e_len bytes +
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const (
+	// header + body length
+	headerFixedLength = 19
+)
+
+type rmqCodec struct {}
+
+// encodeHeader
+func (c *rmqCodec) encodeHeader(command *remotingCommand)  ([]byte, error) {
+	extBytes, err := c.encodeMaps(command.ExtFields)
+	if err != nil {
+		return nil, err
+	}
+
+	buf := bytes.NewBuffer(make([]byte, headerFixedLength + len(command.Remark) + len(extBytes)))
+
+	// request code, length is 2 bytes
+	err = binary.Write(buf, binary.BigEndian, command.Code)
+	if err != nil {
+		return nil, err
+	}
+
+	// language flag, length is 1 byte
+	err = binary.Write(buf, binary.BigEndian, languageCode)
+	if err != nil {
+		return nil, err
+	}
+
+	// version flag, length is 2 bytes
+	err = binary.Write(buf, binary.BigEndian, command.Version)
+	if err != nil {
+		return nil, err
+	}
+
+	// opaque flag, opaque is request identifier, length is 4 bytes
+	err = binary.Write(buf, binary.BigEndian, command.Opaque)
+	if err != nil {
+		return nil, err
+	}
+
+	// request flag, length is 4 bytes
+	err = binary.Write(buf, binary.BigEndian, command.Flag)
+	if err != nil {
+		return nil, err
+	}
+
+	// remark length flag, length is 4 bytes
+	err = binary.Write(buf, binary.BigEndian, int32(len(command.Remark)))
+	if err != nil {
+		return nil, err
+	}
+
+	// write remark, len(command.Remark) bytes
+	if len(command.Remark) > 0 {
+		err = binary.Write(buf, binary.BigEndian, command.Remark)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	err = binary.Write(buf, binary.BigEndian, int32(len(extBytes)))
+	if err != nil {
+		return nil, err
+	}
+
+	if len(extBytes) > 0 {
+		err = binary.Write(buf, binary.BigEndian, extBytes)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return buf.Bytes(), nil
+}
+
+func (c *rmqCodec) encodeMaps(maps map[string]string) ([]byte, error) {
+	if maps == nil || len(maps) == 0 {
+		return []byte{}, nil
+	}
+	extFieldsBuf := bytes.NewBuffer([]byte{})
+	var err error
+	for key, value := range maps {
+		err = binary.Write(extFieldsBuf, binary.BigEndian, int16(len(key)))
+		if err != nil {
+			return nil, err
+		}
+		err = binary.Write(extFieldsBuf, binary.BigEndian, []byte(key))
+		if err != nil {
+			return nil, err
+		}
+
+		err = binary.Write(extFieldsBuf, binary.BigEndian, int32(len(value)))
+		if err != nil {
+			return nil, err
+		}
+		err = binary.Write(extFieldsBuf, binary.BigEndian, []byte(value))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return extFieldsBuf.Bytes(), nil
+}
+
+func (c *rmqCodec) decodeHeader(data []byte) (*remotingCommand, error) {
+	var err error
+	command := &remotingCommand{}
+	buf := bytes.NewBuffer(data)
+	// int code(~32767)
+	err = binary.Read(buf, binary.BigEndian, &command.Code)
+	if err != nil {
+		return nil, err
+	}
+
+	// LanguageCode language
+
+	var (
+		languageCode byte
+		remarkLen int32
+		extFieldsLen int32
+	)
+	err = binary.Read(buf, binary.BigEndian, &languageCode)
+	if err != nil {
+		return nil, err
+	}
+	//command.Language = languageFlag
+
+	// int version(~32767)
+	err = binary.Read(buf, binary.BigEndian, &command.Version)
+	if err != nil {
+		return nil, err
+	}
+
+	// int opaque
+	err = binary.Read(buf, binary.BigEndian, &command.Opaque)
+	if err != nil {
+		return nil, err
+	}
+
+	// int flag
+	err = binary.Read(buf, binary.BigEndian, &command.Flag)
+	if err != nil {
+		return nil, err
+	}
+
+	// String remark
+	err = binary.Read(buf, binary.BigEndian, &remarkLen)
+	if err != nil {
+		return nil, err
+	}
+
+	if remarkLen > 0 {
+		var remarkData = make([]byte, remarkLen)
+		err = binary.Read(buf, binary.BigEndian, &remarkData)
+		if err != nil {
+			return nil, err
+		}
+		command.Remark = string(remarkData)
+	}
+
+	err = binary.Read(buf, binary.BigEndian, &extFieldsLen)
+	if err != nil {
+		return nil, err
+	}
+
+	if extFieldsLen > 0 {
+		extFieldsData := make([]byte, extFieldsLen)
+		err = binary.Read(buf, binary.BigEndian, &extFieldsData)
+		if err != nil {
+			return nil, err
+		}
+
+		command.ExtFields = make(map[string]string)
+		buf := bytes.NewBuffer(extFieldsData)
+		var (
+			kLen int16
+			vLen int32
+		)
+		for buf.Len() > 0 {
+			err = binary.Read(buf, binary.BigEndian, &kLen)
+			if err != nil {
+				return nil, err
+			}
+
+			key, err := getExtFieldsData(buf, int32(kLen))
+			if err != nil {
+				return nil, err
+			}
+
+			err = binary.Read(buf, binary.BigEndian, &vLen)
+			if err != nil {
+				return nil, err
+			}
+
+			value, err := getExtFieldsData(buf, vLen)
+			if err != nil {
+				return nil, err
+			}
+			command.ExtFields[key] = value
+		}
+	}
+
+	return command, nil
+}
+
+func getExtFieldsData(buff *bytes.Buffer, length int32) (string, error) {
+	var data = make([]byte, length)
+	err := binary.Read(buff, binary.BigEndian, &data)
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
