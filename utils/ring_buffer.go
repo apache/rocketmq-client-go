1a860c8f1e6f60da7f1444242d3e039bc432170d
askuy <askuy@foxmail.com>
2019-03-12 20:50:43 +0800

    ring buffer (#40)

--- utils/ring_buffer.go
+++ utils/ring_buffer.go
@@ -17,48 +17,185 @@
 
 package utils
 
-import "sync"
+import (
+	"runtime"
+	"time"
+	"sync/atomic"
+)
 
-type RingBuffer struct {
-	buf      []byte
-	writePos int
-	readPos  int
-	cap      int
-	rwMutex  sync.RWMutex
-	exitCh   chan interface{}
+// 1.需要能够动态扩容
+// 2.缩容看情况
+// 3.read的时候需要block
+// 4.线程安全
+type RingNodesBuffer struct {
+	writePos uint64
+	readPos  uint64
+	mask  uint64
+
+	nodes nodes
 }
 
-func NewRingBuffer(cap int) *RingBuffer {
-	rb := &RingBuffer{buf: make([]byte, cap), cap: cap}
-	go rb.resize()
+type node struct {
+	position uint64
+	buf     []byte
+}
+
+type nodes []*node
+
+// roundUp takes a uint64 greater than 0 and rounds it up to the next
+// power of 2.
+func roundUp(v uint64) uint64 {
+	v--
+	v |= v >> 1
+	v |= v >> 2
+	v |= v >> 4
+	v |= v >> 8
+	v |= v >> 16
+	v |= v >> 32
+	v++
+	return v
+}
+
+func (rb *RingNodesBuffer) init(size uint64) {
+	size = roundUp(size)
+	rb.nodes = make(nodes, size)
+	for i := uint64(0); i < size; i++ {
+		rb.nodes[i] = &node{position: i}
+	}
+	rb.mask = size - 1 // so we don't have to do this with every put/get operation
+}
+
+func NewRingNodesBuffer(cap uint64) *RingNodesBuffer {
+	rb := &RingNodesBuffer{}
+	rb.init(cap)
+	//go rb.resize()
 	return rb
 }
 
-func (r *RingBuffer) Write(b []byte) error {
-	// TODO
+func (r *RingNodesBuffer) Write(b []byte) error {
+	var n *node
+	var dif uint64
+	pos := atomic.LoadUint64(&r.writePos)
+	i := 0
+L:
+	for {
+		// pos 16 seq 1.     0001 0000    00001111   0001 1111
+		n = r.nodes[pos&r.mask]
+		seq := atomic.LoadUint64(&n.position)
+		switch dif = seq - pos; {
+		case dif == 0:
+			if atomic.CompareAndSwapUint64(&r.writePos, pos, pos+1) {
+				break L
+			}
+		default:
+			pos = atomic.LoadUint64(&r.writePos)
+		}
+		if i == 10000 {
+			runtime.Gosched() // free up the cpu before the next iteration
+			i = 0
+		} else {
+			i++
+		}
+	}
+
+	n.buf = b
+	atomic.StoreUint64(&n.position, pos+1)
 	return nil
 }
 
-func (r *RingBuffer) Read(p []byte) (n int, err error) {
-
-	if r.Size() >= len(p) {
-		copy(p, r.buf[r.readPos:r.readPos+len(p)])
-		r.readPos += len(p)
-
+// 直接返回数据
+func (r *RingNodesBuffer) Read(timeout time.Duration) (data []byte, err error) {
+	var (
+		node     *node
+		pos   = atomic.LoadUint64(&r.readPos)
+		start time.Time
+		dif uint64
+	)
+	if timeout > 0 {
+		start = time.Now()
 	}
-
-	// TODO waiting data...
-	return 0, err
+	i := 0
+L:
+	for {
+		node = r.nodes[pos&r.mask]
+		seq := atomic.LoadUint64(&node.position)
+		switch dif = seq - (pos + 1); {
+		case dif == 0:
+			if atomic.CompareAndSwapUint64(&r.readPos, pos, pos+1) {
+				break L
+			}
+		default:
+			pos = atomic.LoadUint64(&r.readPos)
+		}
+		if timeout > 0 && time.Since(start) >= timeout {
+			return
+		}
+		if i == 10000 {
+			runtime.Gosched() // free up the cpu before the next iteration
+			i = 0
+		} else {
+			i++
+		}
+	}
+	data = node.buf
+	atomic.StoreUint64(&node.position, pos+r.mask+1)
+	return
 }
 
-func (r *RingBuffer) Size() int {
-	return r.writePos - r.readPos
+// 知道大小，传进去解析
+func (r *RingNodesBuffer) ReadBySize(data []byte,timeout time.Duration) (n int, err error) {
+	var (
+		node     *node
+		pos   = atomic.LoadUint64(&r.readPos)
+		start time.Time
+		dif uint64
+	)
+	i := 0
+	if timeout > 0 {
+		start = time.Now()
+	}
+L:
+	for {
+		node = r.nodes[pos&r.mask]
+		seq := atomic.LoadUint64(&node.position)
+		switch dif = seq - (pos + 1); {
+		case dif == 0:
+			if atomic.CompareAndSwapUint64(&r.readPos, pos, pos+1) {
+				break L
+			}
+		default:
+			pos = atomic.LoadUint64(&r.readPos)
+		}
+		if timeout > 0 && time.Since(start) >= timeout {
+			return
+		}
+		if i == 10000 {
+			runtime.Gosched() // free up the cpu before the next iteration
+			i = 0
+		} else {
+			i++
+		}
+	}
+	n = copy(data,node.buf)
+	atomic.StoreUint64(&node.position, pos+r.mask+1)
+	return
 }
 
-func (r *RingBuffer) Destroy() {
+
+func (r *RingNodesBuffer) Size() uint64 {
+	return atomic.LoadUint64(&r.writePos) - atomic.LoadUint64(&r.readPos)
 
 }
 
-func (r *RingBuffer) resize() {
+// Cap returns the capacity of this ring buffer.
+func (rb *RingNodesBuffer) Cap() uint64 {
+	return uint64(len(rb.nodes))
+}
+
+func (r *RingNodesBuffer) Destroy() {
+
+}
+
+func (r *RingNodesBuffer) resize() {
 	// TODO
 }
