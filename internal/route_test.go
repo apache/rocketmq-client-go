25f403678ec98ca65678b89bdb4b40224bef68dd
兰园望月 <1376030681@qq.com>
2019-07-15 09:05:16 +0800

    [ISSUE #75] Add internal/route.go unit tests (#116)
    * add route test
    
    * test AddBroker and repair encodeMessages

--- internal/route_test.go
+++ internal/route_test.go
@@ -18,9 +18,118 @@ limitations under the License.
 package internal
 
 import (
+	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
 
+const (
+	topic = "TopicTest"
+)
+
+func init() {
+	srvs := []string{"127.0.0.1:9876"}
+	namesrv, err := NewNamesrv(srvs...)
+	if err != nil {
+		panic("register namesrv fail")
+	}
+	RegisterNamsrv(namesrv)
+}
+
+func TestAddBroker(t *testing.T) {
+	Convey("Given a starting topic", t, func() {
+		remoteRouteData, err := queryTopicRouteInfoFromServer(topic)
+		So(err, ShouldBeNil)
+		AddBroker(remoteRouteData)
+
+		Convey("brokerData from brokerAddressesMap by brokeName should be deep equal remoteBrokeData from server", func() {
+			for _, remoteBrokerData := range remoteRouteData.BrokerDataList {
+				brokerName := remoteBrokerData.BrokerName
+				brokerData, ok := brokerAddressesMap.Load(brokerName)
+				So(ok, ShouldBeTrue)
+				So(brokerData, ShouldResemble, remoteBrokerData)
+			}
+		})
+	})
+}
+
 func TestUpdateTopicRouteInfo(t *testing.T) {
-	UpdateTopicRouteInfo("test")
+	Convey("Given a starting topic", t, func() {
+		updatedRouteData := UpdateTopicRouteInfo(topic)
+
+		Convey("updatedRouteData should be deep equal remoteRouteData", func() {
+			remoteRouteData, err := queryTopicRouteInfoFromServer(topic)
+			So(err, ShouldBeNil)
+			So(updatedRouteData, ShouldResemble, remoteRouteData)
+		})
+		Convey("updatedRouteData should be deep equal localRouteData", func() {
+			localRouteData, exist := routeDataMap.Load(topic)
+			So(exist, ShouldBeTrue)
+			So(updatedRouteData, ShouldResemble, localRouteData)
+		})
+	})
+}
+
+func TestFindBrokerAddrByTopic(t *testing.T) {
+	Convey("Given a starting topic", t, func() {
+		addr := FindBrokerAddrByTopic(topic)
+		remoteRouteData, err := queryTopicRouteInfoFromServer(topic)
+		So(err, ShouldBeNil)
+		brokerAddrList := remoteRouteData.BrokerDataList
+
+		Convey("addr from FindBrokerAddrByTopic should be contained in remoteRouteData", func() {
+			flag := false
+			for _, brokerData := range brokerAddrList {
+				for _, ba := range brokerData.BrokerAddresses {
+					if ba == addr {
+						flag = true
+						break
+					}
+				}
+			}
+			So(flag, ShouldBeTrue)
+		})
+	})
+}
+
+func TestFindBrokerAddrByName(t *testing.T) {
+	Convey("Given a starting topic", t, func() {
+		remoteRouteData, err := queryTopicRouteInfoFromServer(topic)
+		So(err, ShouldBeNil)
+		brokerAddrList := remoteRouteData.BrokerDataList
+
+		Convey("addr from FindBrokerAddrByName should be equal remoteBrokerAddr from server", func() {
+			for _, brokerData := range brokerAddrList {
+				brokerName := brokerData.BrokerName
+				addr := FindBrokerAddrByName(brokerName)
+				remoteBrokerAddr := brokerData.BrokerAddresses[MasterId]
+				So(addr, ShouldEqual, remoteBrokerAddr)
+			}
+		})
+	})
+}
+
+func TestFindBrokerAddressInSubscribe(t *testing.T) {
+	Convey("Given a starting topic", t, func() {
+		remoteRouteData, err := queryTopicRouteInfoFromServer(topic)
+		So(err, ShouldBeNil)
+		brokerAddrList := remoteRouteData.BrokerDataList
+
+		Convey("range BrokerAddress and compare them in turn", func() {
+			for _, brokerData := range brokerAddrList {
+				brokerName := brokerData.BrokerName
+				for id, ba := range brokerData.BrokerAddresses {
+					findBrokerRes := FindBrokerAddressInSubscribe(brokerName, id, true)
+					res := &FindBrokerResult{
+						BrokerAddr:    ba,
+						Slave:         false,
+						BrokerVersion: findBrokerVersion(brokerName, ba),
+					}
+					if id != MasterId {
+						res.Slave = true
+					}
+					So(findBrokerRes, ShouldResemble, res)
+				}
+			}
+		})
+	})
 }
