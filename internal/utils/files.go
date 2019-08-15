8b2892b6d5d51d502064da5675345cb5a81f6a09
胖梁 <418094911@qq.com>
2019-08-15 13:09:25 +0800

    try to fix ci fail

--- internal/utils/files.go
+++ internal/utils/files.go
@@ -45,10 +45,8 @@ func MakeFileIfNotExist(path string) error {
 	info, err := os.Stat(path)
 	if err != nil {
 		if os.IsNotExist(err) {
-			if err = os.MkdirAll(path, 0755); err != nil {
-				return err
-			}
-			return nil
+			err = os.MkdirAll(path, 0755)
+			return err
 		}
 	}
 	if !info.IsDir() {
