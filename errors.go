303ae26b2348e899930222d3a4e90e20b2531a17
wenfeng <sxian.wang@gmail.com>
2019-09-10 12:27:52 +0800

    [ISSUE #184]Fix errors of using internal codes in the exported snippet

--- errors.go
+++ errors.go
@@ -18,22 +18,13 @@ limitations under the License.
 package rocketmq
 
 import (
-	"github.com/apache/rocketmq-client-go/rlog"
-
 	"github.com/pkg/errors"
 )
 
 var (
 	// ErrRequestTimeout for request timeout error
 	ErrRequestTimeout = errors.New("request timeout")
-
-	ErrMQEmpty = errors.New("MessageQueue is nil")
-	ErrOffset  = errors.New("offset < 0")
-	ErrNumbers = errors.New("numbers < 0")
+	ErrMQEmpty        = errors.New("MessageQueue is nil")
+	ErrOffset         = errors.New("offset < 0")
+	ErrNumbers        = errors.New("numbers < 0")
 )
-
-func CheckError(action string, err error) {
-	if err != nil {
-		rlog.Errorf("%s error: %s", action, err.Error())
-	}
-}
