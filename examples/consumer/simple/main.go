32b525e88d2338684db65a28265474648a3f0f65
KiloG <33695125+JOJO0527@users.noreply.github.com>
2022-03-11 21:19:31 +0800

    fix typo
    fix typo of method  consumer.WithNsResovler

--- examples/consumer/simple/main.go
+++ examples/consumer/simple/main.go
@@ -31,7 +31,7 @@ import (
 func main() {
 	c, _ := rocketmq.NewPushConsumer(
 		consumer.WithGroupName("testGroup"),
-		consumer.WithNsResolver(primitive.NewPassthroughResolver([]string{"127.0.0.1:9876"})),
+		consumer.WithNsResovler(primitive.NewPassthroughResolver([]string{"127.0.0.1:9876"})),
 	)
 	err := c.Subscribe("test", consumer.MessageSelector{}, func(ctx context.Context,
 		msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
