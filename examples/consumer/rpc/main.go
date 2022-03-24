bcb64b63acb1d4c3404ec2c4830a497bff451872
Changeden <changeden520@gmail.com>
2022-03-24 11:10:04 +0800

    add License

--- examples/consumer/rpc/main.go
+++ examples/consumer/rpc/main.go
@@ -1,9 +1,27 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
 	"context"
 	"fmt"
-	consumer2 "github.com/apache/rocketmq-client-go/v2/consumer"
+
+	"github.com/apache/rocketmq-client-go/v2/consumer"
 	"github.com/apache/rocketmq-client-go/v2/primitive"
 	"github.com/apache/rocketmq-client-go/v2/producer"
 )
@@ -31,17 +49,17 @@ func main() {
 	}
 
 	// create consumer
-	consumer, err := consumer2.NewPushConsumer(
-		consumer2.WithGroupName(consumerGroup),
-		consumer2.WithConsumeFromWhere(consumer2.ConsumeFromLastOffset),
-		consumer2.WithPullInterval(0),
-		consumer2.WithNsResolver(primitive.NewPassthroughResolver([]string{"127.0.0.1:9876"})),
+	c, err := consumer.NewPushConsumer(
+		consumer.WithGroupName(consumerGroup),
+		consumer.WithConsumeFromWhere(consumer.ConsumeFromLastOffset),
+		consumer.WithPullInterval(0),
+		consumer.WithNsResolver(primitive.NewPassthroughResolver([]string{"127.0.0.1:9876"})),
 	)
-	err = consumer.Subscribe(topic, consumer2.MessageSelector{
-		Type:       consumer2.TAG,
+	err = c.Subscribe(topic, consumer.MessageSelector{
+		Type:       consumer.TAG,
 		Expression: "*",
 	}, func(ctx context.Context,
-		msgs ...*primitive.MessageExt) (consumer2.ConsumeResult, error) {
+		msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
 		fmt.Printf("subscribe callback: %v \n", msgs)
 		for _, msg := range msgs {
 			fmt.Printf("handle message: %s", msg.String())
@@ -74,13 +92,13 @@ func main() {
 			}
 			fmt.Printf("reply to %s , %s \n", replyTo, replyResult.String())
 		}
-		return consumer2.ConsumeSuccess, nil
+		return consumer.ConsumeSuccess, nil
 	})
 	if err != nil {
 		fmt.Printf("error: %s\n", err)
 		return
 	}
-	err = consumer.Start()
+	err = c.Start()
 	if err != nil {
 		fmt.Printf("error: %s\n", err)
 		return
