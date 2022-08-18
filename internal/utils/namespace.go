241b453bc449254e05091caac03d90630de1dae5
wumu.hx <wumu.hx@antfin.com>
2022-08-18 17:42:43 +0800

    (feat): add user defined topic dimension consumer limiter

--- internal/utils/namespace.go
+++ internal/utils/namespace.go
@@ -3,6 +3,8 @@ package utils
 import "strings"
 
 const namespaceSeparator = "%"
+const retryPrefix = "%RETRY%"
+const dlqPrefix = "%DLQ%"
 
 func WrapNamespace(namespace, resourceWithOutNamespace string) string {
 	if IsEmpty(namespace) || IsEmpty(resourceWithOutNamespace) {
@@ -21,4 +23,23 @@ func isAlreadyWithNamespace(resource, namespace string) bool {
 		return false
 	}
 	return strings.Contains(resource, namespace+namespaceSeparator)
+}
+
+func WithoutNamespace(resource string) string {
+	if len(resource) == 0 {
+		return resource
+	}
+	resourceWithoutNamespace := ""
+	if strings.HasPrefix(resource, retryPrefix) {
+		resourceWithoutNamespace += retryPrefix
+	} else if strings.HasPrefix(resource, dlqPrefix) {
+		resourceWithoutNamespace += dlqPrefix
+	}
+	index := strings.LastIndex(resource, namespaceSeparator)
+	if index > 0 {
+		resourceWithoutNamespace += resource[index+1:]
+	} else {
+		resourceWithoutNamespace = resource
+	}
+	return resourceWithoutNamespace
 }
