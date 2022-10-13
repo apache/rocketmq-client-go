cbbe49c2bebbf7c556b6567f773f9be9a15331a8
筱瑜 <maoyu.dmy@alibaba-inc.com>
2022-10-14 00:15:03 +0800

    init crCh after topic with namespace instead of before which will cause consumption blocked; opt on log

--- consumer/push_consumer.go
+++ consumer/push_consumer.go
@@ -260,12 +260,11 @@ func (pc *pushConsumer) Subscribe(topic string, selector MessageSelector,
 		return errors2.ErrStartTopic
 	}
 
-	if _, ok := pc.crCh[topic]; !ok {
-		pc.crCh[topic] = make(chan struct{}, pc.defaultConsumer.option.ConsumeGoroutineNums)
-	}
-
 	if pc.option.Namespace != "" {
 		topic = pc.option.Namespace + "%" + topic
+	}
+	if _, ok := pc.crCh[topic]; !ok {
+		pc.crCh[topic] = make(chan struct{}, pc.defaultConsumer.option.ConsumeGoroutineNums)
 	}
 	data := buildSubscriptionData(topic, selector)
 	pc.subscriptionDataTable.Store(topic, data)
