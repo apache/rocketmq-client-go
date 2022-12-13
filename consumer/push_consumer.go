bb043230f972ae67b875e07ab66e5e2dba3e5d3f
Hankunming <1109939087@qq.com>
2022-12-13 20:52:52 +0800

    fix:logic error when reset retry message's origin topic

--- consumer/push_consumer.go
+++ consumer/push_consumer.go
@@ -1054,7 +1054,7 @@ func (pc *pushConsumer) resetRetryAndNamespace(subMsgs []*primitive.MessageExt) 
 	for idx := range subMsgs {
 		msg := subMsgs[idx]
 		retryTopic := msg.GetProperty(primitive.PropertyRetryTopic)
-		if retryTopic == "" && groupTopic == msg.Topic {
+		if retryTopic != "" && groupTopic == msg.Topic {
 			msg.Topic = retryTopic
 		}
 		subMsgs[idx].WithProperty(primitive.PropertyConsumeStartTime, strconv.FormatInt(
