dcb5d3162f06abdc08bda8d68caee55e2f283b65
Tomilla <Tomilla@users.noreply.github.com>
2023-08-20 13:28:08 +0800

    fix(json_int_key): test with more complicated json such as nested json, json in array

--- internal/utils/json_int_key_parser.go
+++ internal/utils/json_int_key_parser.go
@@ -12,6 +12,8 @@ func RectifyJsonIntKeys(input string) string {
 
 	var corrected strings.Builder
 	insideObjectDepth := 0
+	insideArrayDepth := 0
+	inObjectInsideArray := false
 
 	for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
 		text := s.TokenText()
@@ -21,8 +23,22 @@ func RectifyJsonIntKeys(input string) string {
 		} else if text == "}" {
 			insideObjectDepth--
 		}
-
-		if insideObjectDepth >= 1 && tok == scanner.Int {
+		if text == "{" {
+			insideObjectDepth++
+			if insideArrayDepth > 0 {
+				inObjectInsideArray = true
+			}
+		} else if text == "}" {
+			insideObjectDepth--
+			if insideObjectDepth == 0 {
+				inObjectInsideArray = false
+			}
+		} else if text == "[" {
+			insideArrayDepth++
+		} else if text == "]" {
+			insideArrayDepth--
+		}
+		if insideObjectDepth >= 1 && (inObjectInsideArray || insideArrayDepth == 0) && tok == scanner.Int {
 			corrected.WriteString(`"`)
 			corrected.WriteString(text)
 			corrected.WriteString(`"`)
@@ -35,12 +51,21 @@ func RectifyJsonIntKeys(input string) string {
 	return corrected.String()
 }
 
-// RectifyJsonIntKeysByChar  character by character fix JSON int type keys issue
+func _charIsInteger(ch uint8) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func _charIsWhitespace(ch uint8) bool {
+	return ch == ' ' || ch == '\t' || ch == '\n'
+}
+
 func RectifyJsonIntKeysByChar(input string) string {
 	var corrected strings.Builder
 	insideObjectDepth := 0
+	insideArrayDepth := 0
 	insideString := false
 	escaped := false
+	inObjectInsideArray := false
 
 	for i := 0; i < len(input); i++ {
 		ch := input[i]
@@ -55,26 +80,59 @@ func RectifyJsonIntKeysByChar(input string) string {
 			escaped = false // 关闭转义模式
 		}
 
-		// 在非字符串中，且非转义模式时，去判断 Key 是否在 Object 中（计数器表示嵌套的层级）
+		// 在非字符串中，且非转义模式时，去判断:
+		// 1. 当前是否在 Object 中（计数器表示嵌套的层级）
+		// 2. 当前是否处于 Array 中：
+		//   - 对于一般的 Array 中的整数不需要特殊处理
+		//   - 而 Object 嵌套在数组中则依然需要处理
 		if !insideString && !escaped {
 			if ch == '{' {
 				insideObjectDepth++
+				if insideArrayDepth > 0 {
+					inObjectInsideArray = true
+				}
 			} else if ch == '}' {
 				insideObjectDepth--
+				if insideObjectDepth == 0 {
+					inObjectInsideArray = false
+				}
+			} else if ch == '[' {
+				insideArrayDepth++
+			} else if ch == ']' {
+				insideArrayDepth--
 			}
 		}
 
-		// 对象嵌套层级大于1，且不在字符串中，且字符是 0-9 的数字
-		if insideObjectDepth >= 1 && !insideString && (ch >= '0' && ch <= '9') {
-			// 为该字符加上引号，写入到 Buffer 区
-			corrected.WriteByte('"')
-			for i < len(input) && input[i] >= '0' && input[i] <= '9' {
-				corrected.WriteByte(input[i])
-				i++
+		// 进入修正环节的条件
+		// 1. 在对象中
+		// 2. 对象嵌套在数组中，或者，不在数组中
+		// 3. 不在字符串中
+		// 4. 当前字符在 ASCII 码表的整数范围中
+		if insideObjectDepth >= 1 && (inObjectInsideArray || insideArrayDepth == 0) && !insideString && _charIsInteger(ch) {
+			startIdx := i
+			// 连续的整数
+			for i < len(input) && _charIsInteger(input[i]) {
+				i++ // 由于预读了连续的整数，需要更新读数索引
 			}
-			i--
-			corrected.WriteByte('"')
-			continue
+			i-- // 退出循环时，当前字符已经不是整数了
+
+			// Lookahead to see if the next non-whitespace character is a colon
+			// 向前看（这里只是看，不会实际消费字符），跳过空白字符
+			lookahead := i + 1
+			for lookahead < len(input) && (_charIsWhitespace(input[lookahead])) {
+				lookahead++
+			}
+			// 下一字符是 `:`，说明这是一个键，那么为其用引号包围
+			if lookahead < len(input) && input[lookahead] == ':' {
+				corrected.WriteByte('"')
+				corrected.WriteString(input[startIdx : i+1])
+				corrected.WriteByte('"')
+				continue
+			} else { // 否则原样输出
+				// Write the number without quotes
+				corrected.WriteString(input[startIdx : i+1])
+				continue
+			}
 		}
 		// 其他字符，无需处理，直接透传
 		corrected.WriteByte(ch)
--- internal/utils/json_int_key_parser_test.go
+++ internal/utils/json_int_key_parser_test.go
@@ -94,6 +94,30 @@ func TestRectifyJsonIntKeys(t *testing.T) {
 }`,
 			needConv: true,
 		},
+		{
+			// OK: JSON 包含整数数组
+			name:     "ok: Integer value in array",
+			jsonStr:  `{"key": [1, 2, 3, 4, 5]}`,
+			needConv: false,
+		},
+		{
+			// NG: JSON 包含整数数组
+			name:     "ok: Integer value in array",
+			jsonStr:  `{6: [1, 2, 3, 4, 5]}`,
+			needConv: true,
+		},
+		{
+			// OK: JSON 多层
+			name:     "ok: JSON nested",
+			jsonStr:  `{"foo": {"bar": [1, 2, 3, 4, 5]}}`,
+			needConv: false,
+		},
+		{
+			// NG: JSON 多层
+			name:     "ok: JSON nested",
+			jsonStr:  `{"foo": {6: [1, 2, 3, 4, 5]}}`,
+			needConv: true,
+		},
 	}
 
 	for _, testCase := range testCases {
@@ -109,14 +133,178 @@ func TestRectifyJsonIntKeys(t *testing.T) {
 			correctedJSON := funcConv(testCase.jsonStr)
 			// Token 级别的修正函数不需要测试，因为它会把 Token 之间的空白字符剔除掉
 			// 专门用于测试字符级别的修正函数，检查是否前后一致，对于不需要修正的字符串，操作前后应相同
-			if funcName == "RectifyJsonKeysByChar" && !testCase.needConv {
+			if funcName == "RectifyJsonIntKeysByChar" && !testCase.needConv {
+				if correctedJSON != testCase.jsonStr {
+					fmt.Printf("Original  JSON: %s\n", testCase.jsonStr)
+					fmt.Printf("Corrected JSON: %s\n\n", correctedJSON)
+				}
 				assert.True(t, correctedJSON == testCase.jsonStr, "not need convert, but json is not equal after process")
 			}
 
 			var result map[string]interface{}
 			err2 := json.Unmarshal([]byte(correctedJSON), &result)
 			if err2 != nil {
-				fmt.Printf("Func %s Test %s failed. Expected %v, got %v. err %v\n", funcName, testCase.name, testCase.needConv, err2 == nil, err2)
+				fmt.Printf("Func %s Test %s failed. err %v\n", funcName, testCase.name, err2)
+				fmt.Printf("Original  JSON: %s\n", testCase.jsonStr)
+				fmt.Printf("Corrected JSON: %s\n\n", correctedJSON)
+				t.FailNow()
+			}
+			fmt.Printf("%s Test %s passed.\n\n", funcName, testCase.name)
+		}
+	}
+}
+
+func TestRectifyIntKeysInArray(t *testing.T) {
+	testCases := []struct {
+		name     string
+		jsonStr  string
+		needConv bool
+	}{
+		{
+			// OK: JSON in Array
+			name:     "ok: object in array",
+			jsonStr:  `[{"1": "val"}]`,
+			needConv: false,
+		},
+		{
+			// NG: JSON in Array
+			name:     "ng: object in array",
+			jsonStr:  `[{"1": "val"}, {2: "val"}]`,
+			needConv: true,
+		},
+		{
+			// OK: JSON in Array(more deep)
+			name:     "ok: object in array(more deep)",
+			jsonStr:  `[{"1": "val"}, {"2": [{"3": [1, 2, 3]}]}]`,
+			needConv: false,
+		},
+		{
+			// NG: JSON in Array(more deep)
+			name:     "ng: object in array(more deep)",
+			jsonStr:  `[{"1": "val"}, {"2": [{3: [1, 2, 3]}]}]`,
+			needConv: true,
+		},
+	}
+	for _, testCase := range testCases {
+		var preLoadResult []map[string]interface{}
+		err := json.Unmarshal([]byte(testCase.jsonStr), &preLoadResult)
+		if testCase.needConv { // 需要转换才能加载，否则直接加载会报错
+			assert.NotNil(t, err, "json is invalid, need convert")
+		} else { // 无需转换，就能直接加载
+			assert.Nil(t, err, "json is valid, not need convert")
+		}
+
+		for funcName, funcConv := range _testFuncMap {
+			correctedJSON := funcConv(testCase.jsonStr)
+			if funcName == "RectifyJsonIntKeysByChar" && !testCase.needConv {
+				assert.True(t, correctedJSON == testCase.jsonStr, "not need convert, but json is not equal after process")
+			}
+
+			var result []map[string]interface{}
+			err2 := json.Unmarshal([]byte(correctedJSON), &result)
+			if err2 != nil {
+				fmt.Printf("Func %s Test %s failed. err %v\n", funcName, testCase.name, err2)
+				fmt.Printf("Original  JSON: %s\n", testCase.jsonStr)
+				fmt.Printf("Corrected JSON: %s\n\n", correctedJSON)
+				t.FailNow()
+			}
+			fmt.Printf("%s Test %s passed.\n\n", funcName, testCase.name)
+		}
+	}
+
+}
+func TestRectifyJsonIntKeysWithSingleInt(t *testing.T) {
+	testCases := []struct {
+		name     string
+		jsonStr  string
+		needConv bool
+	}{
+		{
+			// OK: JSON 单个整数
+			name:     "ok: single int",
+			jsonStr:  `1`,
+			needConv: false,
+		},
+		{
+			// OK: JSON 单个大整数
+			name:     "ok: large single int",
+			jsonStr:  `999999999`,
+			needConv: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		var preLoadResult int
+		err := json.Unmarshal([]byte(testCase.jsonStr), &preLoadResult)
+		if testCase.needConv { // 需要转换才能加载，否则直接加载会报错
+			assert.NotNil(t, err, "json is invalid, need convert")
+		} else { // 无需转换，就能直接加载
+			assert.Nil(t, err, "json is valid, not need convert")
+		}
+
+		for funcName, funcConv := range _testFuncMap {
+			correctedJSON := funcConv(testCase.jsonStr)
+			if funcName == "RectifyJsonIntKeysByChar" && !testCase.needConv {
+				assert.True(t, correctedJSON == testCase.jsonStr, "not need convert, but json is not equal after process")
+			}
+
+			var result int
+			err2 := json.Unmarshal([]byte(correctedJSON), &result)
+			if err2 != nil {
+				fmt.Printf("Func %s Test %s failed. err %v\n", funcName, testCase.name, err2)
+				fmt.Printf("Original  JSON: %s\n", testCase.jsonStr)
+				fmt.Printf("Corrected JSON: %s\n\n", correctedJSON)
+				t.FailNow()
+			}
+			fmt.Printf("%s Test %s passed.\n\n", funcName, testCase.name)
+		}
+	}
+}
+func TestRectifyJsonIntKeysWithArray(t *testing.T) {
+	testCases := []struct {
+		name     string
+		jsonStr  string
+		needConv bool
+	}{
+		{
+			// OK: one number in array
+			name:     "ok: number array",
+			jsonStr:  `[1]`,
+			needConv: false,
+		},
+		{
+			// OK: one large number in array
+			name:     "ok: large int in array",
+			jsonStr:  `[999999999]`,
+			needConv: false,
+		},
+		{
+			// OK: multiple number in array
+			name:     "ok: multiple large int in array",
+			jsonStr:  `[777777777, 888888888, 999999999]`,
+			needConv: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		var preLoadResult []int
+		err := json.Unmarshal([]byte(testCase.jsonStr), &preLoadResult)
+		if testCase.needConv { // 需要转换才能加载，否则直接加载会报错
+			assert.NotNil(t, err, "json is invalid, need convert")
+		} else { // 无需转换，就能直接加载
+			assert.Nil(t, err, "json is valid, not need convert")
+		}
+
+		for funcName, funcConv := range _testFuncMap {
+			correctedJSON := funcConv(testCase.jsonStr)
+			if funcName == "RectifyJsonIntKeysByChar" && !testCase.needConv {
+				assert.True(t, correctedJSON == testCase.jsonStr, "not need convert, but json is not equal after process")
+			}
+
+			var result []int
+			err2 := json.Unmarshal([]byte(correctedJSON), &result)
+			if err2 != nil {
+				fmt.Printf("Func %s Test %s failed. err %v\n", funcName, testCase.name, err2)
 				fmt.Printf("Original  JSON: %s\n", testCase.jsonStr)
 				fmt.Printf("Corrected JSON: %s\n\n", correctedJSON)
 				t.FailNow()
