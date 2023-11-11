b051a67ce2e3e8d93dcd0ae2c4ab641db21ba19a
hezhiwen <hezhiwen.coder@bytedance.com>
2023-11-11 16:37:06 +0800

    Fix encode buf len

--- internal/remote/codec.go
+++ internal/remote/codec.go
@@ -188,7 +188,7 @@ func encode(command *RemotingCommand) ([]byte, error) {
 	}
 
 	frameSize := 4 + len(header) + len(command.Body)
-	buf := bytes.NewBuffer(make([]byte, frameSize))
+	buf := bytes.NewBuffer(make([]byte, frameSize+4))
 	buf.Reset()
 
 	err = binary.Write(buf, binary.BigEndian, int32(frameSize))
